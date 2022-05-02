904386cd7831e42ad2bd24ca34eb331144b8b721
Vladimer96 <83721879+podporin96@users.noreply.github.com>
2022-05-02 11:17:41 +0000

    2homework

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import random
+
 HELP = """
 help - напечатать справку по программе
 add - добавить задачу в список (название задачи запрашиваем у пользователя)
 show - напечатать все добавленные задачи
-end - выйти из программы
+exit - выйти из программы
 random - добавить случайную задачу на дату сегодня"""
-RANDOM_TASK = "Записаться на курс "
+RANDOM_TASKS = ["записаться на курс","Купить продукты","Прочитать книгу","Написать письмо"]
 tasks = {
   
 }
+today=[] 
+tomorrow=[]
+other=[]
 
 run = True
 
@@ -22,7 +27,8 @@ while run:
   command = input("Введите команду:")
   if command =="help":
     print(HELP)
-  elif command =="end":
+  elif command =="exit":
+    print("Спасибо за использование! До свидания!")
     break  
   elif command == "show":
     date = input("Введите дату для отображения списка задач: ")
@@ -36,8 +42,17 @@ while run:
     task = input("Введите название задачи: ")
     date = input("Введите дату для добавления задачи: ")
     add_todo(date, task)
+    # if date == "Сегодня":
+    #   today.append(task)
+    # elif date == "Завтра":
+    #   tomorrow.append(task)
+    # else:
+    #   other.append(task)
+    # дя 2-го ДЗ
   elif command =="random":
-    add_todo("Сегодня", RANDOM_TASK)
+    task = random.choice(RANDOM_TASKS)
+    add_todo("Сегодня", task)
   else:
     print("Неизвестная команда!")
-print("До свидания") 
+    # print(today, tomorrow, other)
+ 
